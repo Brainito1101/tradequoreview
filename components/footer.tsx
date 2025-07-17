ed86e97e24086f3b26dbd8c4ad2e4eed5f1cc73c
Nidhi Mevada <accounts@brainito.com>
2025-07-17 21:32:00 +0530

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -47,7 +47,7 @@ export default function Footer() {
         </div>
 
         <div className="border-t border-gray-800  flex flex-col md:flex-row justify-between items-center">
-          <p className="text-gray-400 text-sm">© {new Date().getFullYear()} ReviewHub. All rights reserved.</p>
+          <p className="text-gray-400 text-sm">© {new Date().getFullYear()} TradeQuo Review. All rights reserved.</p>
           <div className="flex space-x-6 mt-4 md:mt-0">
             <Link href="#" className="text-gray-400 hover:text-white transition-colors">
               Privacy
